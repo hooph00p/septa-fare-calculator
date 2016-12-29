2c8109f6d5fc4125a3fe96db7103afb1d53aaebd
hooph00p <thejaredhooper@gmail.com>
2016-12-28 19:30:09 -0500

    Finished up functionality, now to refine the appearance.

--- main.js
+++ main.js
@@ -1,22 +1,58 @@
 $(document).ready(function () {
-    var fares = {};
+    var state = {
+        data: {},
+        cost: 0
+    };
 
     function update() {
-        console.log('update');
-        var zone = $('#zone').val();
-        var time = $('#time').val();
-        var purchase_method = $('input[name="purchase_method"]').val();
-        console.log("zone:", zone);
-        console.log("time:", time);
-        console.log("purchase_method:", purchase_method);
+        // Object used to store state
+        var updating = {
+            zone: $('.septa-widget [name="zone"]').val(),
+            type: $('.septa-widget [name="type"]').val(),
+            purchase: $('.septa-widget [name="purchase"]:checked').val(),
+            trips: $('.septa-widget [name="trips"]').val()
+        };
+
+        console.log('State change:', updating);
+
+        /**
+         * Cost calculation:
+         * Find the zone, find the fare, then use a multiplier
+         * to determine the purchasing increment.
+         * 
+         * Iterating through everything because it's simpler.
+         */
+        state.data.zones.forEach(function (zone) {
+            if (zone.zone == updating.zone) {
+                zone.fares.forEach(function (fare) {
+                    if (fare.type == updating.type) {
+                        var multiplier = 1;
+                        while (multiplier * fare.trips < updating.trips) { multiplier++ }
+                        state.cost = fare.price * multiplier;
+                        state.current = fare;
+                        $('.septa-widget #cost').text("$" + state.cost.toFixed(2));
+
+                    }
+                });
+            }
+        });
+
+        /**
+         * Helper text:
+         * Switch on info for the corresponding fields.
+         */
+        $('.septa-widget #purchase-helper').text(state.data.info[updating.purchase]);
+        $('.septa-widget #type-helper').text(state.data.info[updating.type]);
     }
 
-    $('.septa-widget input').change(update);
+    // Listen to all of our widget inputs
+    $('.septa-widget input, .septa-widget select').change(update);
 
+    // Get the fares data, throw that in our state object
     $.ajax({
         url: './fares.json',
-        success: function (data) {
-            fares = JSON.parse(data);
+        success: function (response) {
+            state.data = JSON.parse(response);
             update();
         }
     })
